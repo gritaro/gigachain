6d355ec362a544cd78ac87855fd5756af2b09d9b
gritaro <gritaro@gmail.com>
2024-05-16 19:37:09 +0700

    0.1.7 HA compatibility fix
    default conversation agent instantiation method was changed

--- custom_components/gigachain/__init__.py
+++ custom_components/gigachain/__init__.py
@@ -5,7 +5,7 @@ from typing import Literal
 
 from home_assistant_intents import get_languages
 from homeassistant.components import conversation
-from homeassistant.components.conversation import agent
+from homeassistant.components.conversation import agent_manager
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent, template
@@ -53,9 +53,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = _client
     _agent = GigaChatAI(hass, entry)
-    await _agent.async_initialize(
-        hass.data.get("conversation_config")
-    )
     conversation.async_set_agent(hass, entry, _agent)
     return True
 
@@ -67,13 +64,13 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-class GigaChatAI(conversation.DefaultAgent):
+class GigaChatAI(conversation.AbstractConversationAgent):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         """Initialize the agent."""
-        super().__init__(hass)
         self.hass = hass
         self.entry = entry
         self.history: dict[str, list[BaseMessage]] = {}
+        self.default_agent = agent_manager.async_get_agent(hass, None)
 
     @property
     def supported_languages(self) -> list[str] | Literal["*"]:
@@ -81,8 +78,8 @@ class GigaChatAI(conversation.DefaultAgent):
         return get_languages()
 
     async def async_process(
-            self, user_input: agent.ConversationInput
-    ) -> agent.ConversationResult:
+            self, user_input: conversation.ConversationInput
+    ) -> conversation.ConversationResult:
         """Process a sentence."""
         raw_prompt = self.entry.options.get(CONF_PROMPT, DEFAULT_PROMPT)
         chat_history_enabled = self.entry.options.get(CONF_CHAT_HISTORY, DEFAULT_CHAT_HISTORY)
@@ -100,12 +97,12 @@ class GigaChatAI(conversation.DefaultAgent):
         use_builtin_sentences = self.entry.options.get(CONF_PROCESS_BUILTIN_SENTENCES,
                                                        DEFAULT_PROCESS_BUILTIN_SENTENCES)
         if use_builtin_sentences:
-            default_agent_response = await super(GigaChatAI, self).async_process(user_input)
+            default_agent_response = await self.default_agent.async_process(user_input)
 
             if default_agent_response.response.intent:
                 messages.append(AIMessage(content=default_agent_response.response.speech.get("plain").get("speech")))
                 self.history[conversation_id] = messages
-                return agent.ConversationResult(
+                return conversation.ConversationResult(
                     conversation_id=conversation_id, response=default_agent_response.response
                 )
 
@@ -120,7 +117,7 @@ class GigaChatAI(conversation.DefaultAgent):
                 intent.IntentResponseErrorCode.UNKNOWN,
                 f"Houston we have a problem: {err}",
             )
-            return agent.ConversationResult(
+            return conversation.ConversationResult(
                 conversation_id=conversation_id, response=response
             )
 
@@ -131,7 +128,7 @@ class GigaChatAI(conversation.DefaultAgent):
         response = intent.IntentResponse(language=user_input.language)
         response.async_set_speech(res.content)
         LOGGER.debug(response)
-        return agent.ConversationResult(
+        return conversation.ConversationResult(
             conversation_id=conversation_id, response=response
         )
 
